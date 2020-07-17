6100a035b6985b8585d06d0f62dd23cf1c815593
Jordan <iraborjordan1@gmail.com>
2020-07-17 18:00:19 +0100

    included styles for editor

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,5 +1,4 @@
-import { TimelineLite } from 'gsap';
-import jQuery from 'jquery'
+import { TweenMax } from 'gsap';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './../../services/auth.service';
@@ -7,77 +6,45 @@ import { AuthService } from './../../services/auth.service';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss'],
+  styleUrls: ['./auth.component.scss']
 })
+
 export class AuthComponent implements OnInit {
-  constructor(public authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {}
+  constructor(public authService: AuthService, private router: Router) { }
 
-  startAnimation(): void {
-    // set some global properties
-    TweenLite.set('.wrap', { perspective: 1000 });
-    TweenLite.set('.inner', { transformStyle: 'preserve-3d' });
-    TweenLite.set('.back', { rotationX: -90 });
-    TweenLite.set(['.back', '.front'], {
-      backfaceVisibility: 'hidden',
-      transformOrigin: '50% 0',
-    });
+  ngOnInit(): void {
+this.startAnimation()
+  }
 
+  startAnimation():void{
 
-    jQuery(($: any) => {
-
-    // loop through each element
-    $('.wrapper').each(function (i, el) {
-      // set some individual properties
-      //TweenMax.set($(el).find('.back'), {backgroundColor:'#' + Math.floor(Math.random() * 16777215).toString(16)});
-
-      // create a timeline for this element in paused state
-      var tl = new TimelineMax({ paused: true });
-
-      // create your tween of the timeline in a variable
-      var t = tl
-        .set(el, { willChange: 'transform' })
-        //.set($(el).find('.wrap'), {zIndex: 2, overwrite:"all"})
-        .to(
-          $(el).find('.inner'),
-          0.53,
-          {
-            y: '-40px',
-            rotationX: 90,
-            z: 0.01,
-            zIndex: 2,
-            overwrite: 'all',
-            ease: Back.easeOut,
-          },
-          0
-        );
-
-      // store the tween timeline in the javascript DOM node
-      el.animation = t;
-
-      //create the event handler
-      $(el)
-        .on('mouseenter', function () {
-          this.animation.play();
-        })
-        .on('mouseleave', function () {
-          this.animation.reverse();
-        });
-    });
-  
-
-
+    if (document.readyState === "interactive" || document.readyState === "complete") {
+      resolve();
+    } else {
+      window.addEventListener("DOMContentLoaded", resolve);
+    }
+    
+    function resolve() {
+      
+      document.body.removeAttribute("unresolved");
+      
+      TweenMax.from(".container", {
+        y: 500,
+        opacity: 0,
+        duration: 1.2,
+      });
+    }
+    
   }
 
   login(): void {
-    this.authService
-      .login()
+    this.authService.login()
       .then(() => {
         this.router.navigate(['editor']);
-      })
-      .catch((err) => {
+      }).catch(err => {
         console.error(err);
       });
   }
+
 }
