536542db32e0eb1645d9eec9fae460694745e54d
Jordan <iraborjordan1@gmail.com>
2020-07-17 22:50:13 +0100

    deployment for Firebase Hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCtYzl4wbXA7R-Ud_rF-pUfRFfpi6HT4co',
+    authDomain: 'edtech-test-7182a.firebaseapp.com',
+    databaseURL: 'https://edtech-test-7182a.firebaseio.com/',
+    projectId: 'edtech-test-7182a',
+  }
 };
